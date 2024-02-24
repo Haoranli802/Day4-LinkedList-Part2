cb02ecabc38fd6d43d26ce6fe55e4a876f52a82d
Haoran Li (Evan) <65979806+Haoranli802@users.noreply.github.com>
2024-02-24 16:01:14 +0800

    Update main.java

--- main.java
+++ main.java
@@ -20,3 +20,22 @@ class Solution {
     }
 }
 //Time: O(N) Space:O(1)
+
+
+//LeetCode 19
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode slow = dummy, fast = dummy;
+        for(int i = 0; i < n; i++){
+            fast = fast.next;
+        }
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
+// Time: O(N) Space: O(1)
